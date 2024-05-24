55308313874df0fb10ff4b087d7b1cab1947a245
vanshd15 <163154582+vanshd15@users.noreply.github.com>
2024-05-24 16:19:24 +0530

    Add files via upload

---
+++ test.ts
@@ -0,0 +1,20 @@
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ToDo List';
+  list:any[]=[];
+  addTask(item:string)
+  {
+    
+    this.list.push({id:this.list.length,name:item});
+    console.warn(this.list)
+  }
+  removeTask(id:number)
+  {
+    console.warn(id)
+    this.list=this.list.filter(item=>item.id!==id)
+  }
+}
